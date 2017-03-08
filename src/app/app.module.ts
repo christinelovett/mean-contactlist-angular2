710888488996cef8226de1ae0ffe868ac5ab4977
Christine Lovett <clovett07@gmail.com>
2017-03-08 12:06:39 -0500

    Added log in and sign up pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { SignUpComponent } from './contacts/sign-up/sign-up.component';
+import { LogInComponent } from './contacts/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailsComponent,
-    ContactListComponent
+    ContactListComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
